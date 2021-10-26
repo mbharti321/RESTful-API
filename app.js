4c52a746af9bdd8324a6913a554d5efc24d09079
mbharti321 <mbharti321@gmail.com>
2021-10-26 18:12:48 +0530

    add all remaining http word request

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ const Article = new mongoose.model("Article", articlesSchema);
 app.get("/", function (req, res) {
       res.send("Hello, Check for articles domain");
 });
+
+// handeling get request
 app.get("/articles", function (req, res) {
     //   res.send("Hello");
     Article.find(function (err, foundArticles) {
@@ -38,6 +40,45 @@ app.get("/articles", function (req, res) {
         }
     });
 });
+
+
+// handeling post request
+app.post("/articles", function(req, res){
+    // console.log(req.body.title);
+    // console.log(req.body.content);    
+    const article = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+    article.save(function(err){
+        if(err){
+            res.send(err);
+        }else{
+            res.send("Successfully posted!!!");
+        }
+    });
+})
+
+// handeling delete request
+// get deleted data https://github.com/londonappbrewery/Build-Your-Own-RESTful-API
+app.delete("/articles", function(req, res){
+    // console.log("delete");
+    Article.deleteMany({},function(err){
+        if(err){
+            res.send(err);
+        }else{
+            res.send("Successfully deleted!!!");
+        }
+    })
+})
+
+// app.route("/articles"){
+//     .
+// }
+
+
+
+
 
 app.listen(3000, function () {
     console.log("Server Started!!\nListening to port: http://localhost:3000/")
